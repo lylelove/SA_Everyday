93a314ce4034210d6a0b471532fe0f524b9491da
lyle <lylelove@163.com>
2022-08-19 18:32:17 +0800

    update

--- today_data_load.py
+++ today_data_load.py
@@ -1,14 +1,16 @@
 import json
+import time
 
+date_name = time.strftime("%Y_%m_%d", time.localtime())
 sa_block = ['深a', '创业板', '沪a', '科创版']
 for d_i in range(4):
-    with open('today_data' + str(d_i) + '.json', 'r') as read_file:
+    with open('data/json/' + str(sa_block[d_i]) + str(date_name) + '.json', 'r') as read_file:
         temp = json.load(read_file)
-        with open('今日' + sa_block[d_i] + '.csv', 'w', encoding="utf8") as f:
+        with open('data/csv/' + sa_block[d_i] + str(date_name) + '.csv', 'w', encoding="utf8") as f:
             f.write(
                 '股票代码,名称,板块,时间,今收,跌涨幅,跌涨额,成交量,成交额,振幅,最高,最低,今开,换手率,行业板块,地区板块,市盈率,市净率\n')
             for i in range(len(temp)):
-                f.write(temp[i][0] + ',' + temp[i][1]+',' + sa_block[d_i])
+                f.write(temp[i][0] + ',' + temp[i][1] + ',' + sa_block[d_i])
                 for j in range(len(temp[i][2])):
                     f.write(',' + str(temp[i][2][j]))
                 f.write('\n')
--- today_get.py
+++ today_get.py
@@ -1,9 +1,10 @@
 import json
 import time
 import requests
-
+sa_block = ['深a', '创业板', '沪a', '科创版']
 code_name = []
 date = time.strftime("%Y-%m-%d", time.localtime())
+date_name = time.strftime("%Y_%m_%d", time.localtime())
 hd = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                     'Chrome/99.0.4844.82 Safari/537.36'}
 url1 = 'http://36.push2.eastmoney.com/api/qt/clist/get?pn=1&pz=3000&po=0&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&wbp2u=|0|0|0|web&fid=f12&fs=m:0+t:6,m:0+t:80&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f100,f102,f128,f136,f115,f152'
@@ -20,7 +21,7 @@ for i in range(len(url)):
     except:
         continue
     data_all = []
-    with open('today_data' + str(i) + '.json', 'w') as f:
+    with open('data/json/' + str(sa_block[i])+str(date_name) + '.json', 'w') as f:
         for item in temp:
             data_all.append([item['f12'], item['f14'],
                              [date, item['f2'], item['f3'], item['f4'], item['f5'], item['f6'], item['f7'], item['f15'],
